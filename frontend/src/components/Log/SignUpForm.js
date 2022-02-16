350d113d2fa40748bc9a69469df81825cef537d3
cherokeeal58 <cherokee.al58@outlook.com>
2022-02-16 12:00:09 +0100

    modif inscription

--- frontend/src/components/Log/SignUpForm.js
+++ frontend/src/components/Log/SignUpForm.js
@@ -1,32 +1,95 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+//import SignInForm from './SignInForm';
 import '../SignupForm.scss';
+import axios from 'axios';
 import { Typography } from "@material-ui/core";
 const SignUpForm = () => {
-    return (
-
-        <>
-        <div className="signup_components">
-        <Typography variant="h2" className="h2signup">
-          S'inscrire
-        </Typography>
-        <form className="form_signup">
     
-        <input className="btnType" type="text"  placeholder="Votre prenom" required />
+      const [firstname, setFirstname]= useState('');
+      const [lastname, setLastname]= useState('');
+      const [password, setPassword]= useState('');
+      const[confirmPassword,setConfirmPassword]= useState('');
+      const [email, setEmail]= useState('');
+
+      const handleRegister = async (e) => {
+        e.preventDefault();
+        
+        const confirmPasswordError = document.querySelector(".confirmPassword.error");
+        const emailError = document.querySelector(".email.error");
+        const passwordError = document.querySelector(".password.error");
+        const passwordConfirmError = document.querySelector(".password-confirm.error");
      
-        <input className="btnType" type="text"   placeholder="Votre nom" required />
+    
+        confirmPassword.innerHTML = "";
+    
+    
+        if (password !== confirmPassword) {
+          if (password !== confirmPassword) passwordConfirmError.innerHTML = "Les mots de passe ne correspondent pas";
+    
+        } else {
+          await axios({
+            method: "post",
+            url: `http://localhost:4000/api/user/login`,
+            data: {
+              firstname,
+              lastname,
+              email,
+              password,
+              confirmPassword
+            },
+          })
+            .then((res) => {
+              console.log(res);
+              if (res.data.errors) {
+                confirmPasswordError.innerHTML = res.data.errors.confirmPassword;
+                passwordError.innerHTML = res.data.errors.password;
+                emailError.innerHTML = res.data.errors.email;
+              } 
+            })
+            .catch((err) => console.log(err));
+        }
+      };
+
+    return (
    
-        <input className="btnType" type="password"  placeholder="mot de passe" required />
   
-        <input className="btnType" type="password"  placeholder="confirm mot de passe" required />
- 
-        <input className="btnType" type="email"  placeholder="exemple@groupomania.com" required />
+
+      <div className="signup_components">
+      <Typography variant="h2" className="h2signup">
+        S'inscrire
+      </Typography>
+        
+        <form action="" onSubmit={handleRegister} id="sing-up-form" className="form_signup">
+    
+             <label htmlFor="firstname">Prenom</label>
+            <input className="btnType" type="text" name="firstname" id="firstname" onChange={(e) => setFirstname(e.target.value)} value={firstname} placeholder="Votre prenom" required />
+            <div className='firstname error'></div>
+         
+            <label htmlFor="lastname">Nom</label>
+            <input className="btnType" type="text" name="lastname" id="lastname" onChange={(e) => setLastname(e.target.value)} value={lastname}  placeholder="Votre nom" required />
+            <div className='lastname error'></div>
+
+            <label htmlFor="password">Password</label>
+            <input className="btnType" type="password" name="password" id="password" onChange={(e) => setPassword(e.target.value)} value={password} placeholder="mot de passe" required />
+            <div className='password error'></div>
+
+            
+            <label htmlFor="confirmPassword"> Confirmation Password</label>
+            <input className="btnType" type="password" name="confirmPassword" id="confirmPassword" onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword} placeholder="mot de passe confirmation" required />
+            <div className='password error'></div>
+
+            <label htmlFor="email">Email</label>
+            <input className="btnType" type="text" name="email" id="email" onChange={(e)=> setEmail(e.target.value)} value={email} placeholder="exemple@groupomania.com" required />
+            <div className='email error'></div>
    
         <button type="submit" className="btnSignup">S'inscrire</button>
       </form>
       </div>
-      </>
+      
+    
+      
     );
+  
 };
 
 export default SignUpForm;
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,73 +1,99 @@
 import React, { useState } from "react";
 import { Typography } from "@material-ui/core";
-//css in home.scss
+import '../pages/home.scss';
+import axios from 'axios';
+
 
 
 const SignUp = () => {
-    const [firstname, setFirstname]= useState('');
-    const [lastname, setLastname]= useState('');
-    const [password, setPassword]= useState('');
-    const [email, setEmail]= useState('');
-    const [errors, setErrors] = useState('');
-    let errorsObj = {
-          firstname: '',
-          lastname: '',
-          password: '',
-          email: ''
-    };
-   // const [password, setPassword] = useState(errorObj);
-   function onSignup(e) {
-        e.preventDefault();
-        let error = false;
-        const errorObj = { ...errorsObj};
+  const [firstname, setFirstname]= useState('');
+  const [lastname, setLastname]= useState('');
+  const [password, setPassword]= useState('');
+  const[confirmPassword,setConfirmPassword]= useState('');
+  const [email, setEmail]= useState('');
 
-        if(firstname === '') {
-            errorObj.firstname = 'Votre nom est obligatoire';
-            error = true;
-        }
-        if(lastname === '') {
-            errorObj.lastname = 'Votre nom est obligatoire';
-            error = true;
-        }
-        if(email === '') {
-            errorObj.email = 'Email is Required';
-            error = true;
-        }
-        if(password === '') {
-            errorObj.password = 'Password is Required';
-            error = true;
-        }
-        setErrors(errorObj);
-        if(!error) {
-            console.log('Formulaire à étais bien envoyé');
-           
-        }
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    
+    const confirmPasswordError = document.querySelector(".confirmPassword.error");
+    const emailError = document.querySelector(".email.error");
+    const passwordError = document.querySelector(".password.error");
+    const passwordConfirmError = document.querySelector(".password-confirm.error");
+ 
+
+    confirmPassword.innerHTML = "";
+
+
+    if (password !== confirmPassword) {
+      if (password !== confirmPassword) passwordConfirmError.innerHTML = "Les mots de passe ne correspondent pas";
+
+    } else {
+      await axios({
+        method: "post",
+        url: `http://localhost:4000/api/user/login`,
+        data: {
+          firstname,
+          lastname,
+          email,
+          password,
+          confirmPassword
+        },
+      })
+        .then((res) => {
+          console.log(res);
+          if (res.data.errors) {
+            confirmPasswordError.innerHTML = res.data.errors.confirmPassword;
+            passwordError.innerHTML = res.data.errors.password;
+            emailError.innerHTML = res.data.errors.email;
+          } 
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
+return (
+
+
+
+  <div className="signup_components">
+  <Typography variant="h2" className="h2signup">
+    S'inscrire
+  </Typography>
+    
+    <form action="" onSubmit={handleRegister} id="sing-up-form" className="form_signup">
+
+         <label htmlFor="firstname">Prenom</label>
+        <input className="btnType" type="text" name="firstname" id="firstname" onChange={(e) => setFirstname(e.target.value)} value={firstname} placeholder="Votre prenom" required />
+        <div className='firstname error'></div>
+     
+        <label htmlFor="lastname">Nom</label>
+        <input className="btnType" type="text" name="lastname" id="lastname" onChange={(e) => setLastname(e.target.value)} value={lastname}  placeholder="Votre nom" required />
+        <div className='lastname error'></div>
+
+        <label htmlFor="password">Password</label>
+        <input className="btnType" type="password" name="password" id="password" onChange={(e) => setPassword(e.target.value)} value={password} placeholder="mot de passe" required />
+        <div className='password error'></div>
+
+        
+        <label htmlFor="confirmPassword"> Confirmation Password</label>
+        <input className="btnType" type="password" name="confirmPassword" id="confirmPassword" onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword} placeholder="mot de passe confirmation" required />
+        <div className='password error'></div>
+
+        <label htmlFor="email">Email</label>
+        <input className="btnType" type="text" name="email" id="email" onChange={(e)=> setEmail(e.target.value)} value={email} placeholder="exemple@groupomania.com" required />
+        <div className='email error'></div>
+
+    <button type="submit" className="btnSignup">S'inscrire</button>
+  </form>
+  </div>
+  
+
+  
+);
    }
 
-  return (
-    <div className="signup_components">
-      <Typography variant="h2" className="h2signup">
-        S'inscrire
-      </Typography>
+  
 
-      <form onSubmit={onSignup} className="form_signup">
-    
-        <input className="btnType" type="text" value={firstname} onChange={(e)=> setFirstname(e.target.value)} placeholder="Votre prenom" required />
-        {errors.firstname && <div>{ errors.firstname }</div>}
-        <input className="btnType" type="text" value={lastname} onChange={(e)=> setLastname (e.target.value)}  placeholder="Votre nom" required />
-        {errors.lastname && <div>{ errors.lastname }</div>}
-        <input className="btnType" type="password" value={password} onChange={(e)=> setPassword (e.target.value)}  placeholder="mot de passe" required />
-        {errors.password && <div>{ errors.password }</div>}
-        <input className="btnType" type="password" value={password} onChange={(e)=> setPassword(e.target.value)}  placeholder="confirm mot de passe" required />
-        {errors.password && <div>{ errors.password }</div>}
-        <input className="btnType" type="email" value={email} onChange={(e)=> setEmail(e.target.value)}  placeholder="exemple@groupomania.com" required />
-        {errors.email && <div>{ errors.email }</div>}
-        <button type="submit" className="btnSignup">S'inscrire</button>
-      </form>
-    </div>
-   
-  );
-}
 
 
 export default SignUp;
