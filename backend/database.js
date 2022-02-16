334b3cc7f844435207edef37b616e6906ca789cc
Thierry ALEXANDRE <79103556+Alexandre58@users.noreply.github.com>
2022-02-16 18:36:04 +0100

    Delete database.js

--- backend/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('groupomania-database', 'user', 'pass',{
-    dialect: 'sqlite',
-    host: './dev.sqlite'
-
-})
-
-module.exports = sequelize;
